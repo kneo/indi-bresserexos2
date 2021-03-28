786e8c622d73efb556258d73cd05a979d6d85ab1
Kevin Krüger (kneo) <kkevin@gmx.net>
2021-03-28 16:41:53 +0200

    added astyle format as requested by indi-3rd-party repository

--- TestDataReceivedCallback.cpp
+++ TestDataReceivedCallback.cpp
@@ -5,15 +5,15 @@ using namespace Testing;
 
 TestDataReceivedCallback::TestDataReceivedCallback()
 {
-	
+
 }
-			
+
 TestDataReceivedCallback::~TestDataReceivedCallback()
 {
-	
+
 }
 
 void TestDataReceivedCallback::OnPointingCoordinatesReceived(float right_ascension, float declination)
 {
-	std::cout << "Received data : RA: " << right_ascension << " DEC:" << declination << std::endl;
+    std::cout << "Received data : RA: " << right_ascension << " DEC:" << declination << std::endl;
 }
