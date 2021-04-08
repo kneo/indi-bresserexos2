851f53261a7a693d2e0b8dc0fb6746b36fd4eb5a
Kevin Krüger (kneo) <kkevin@gmx.net>
2021-04-08 13:54:32 +0200

    changed string increased version number

--- BresserExosIIGoToDriver.cpp
+++ BresserExosIIGoToDriver.cpp
@@ -83,7 +83,7 @@ bool BresserExosIIDriver::initProperties()
 
     addDebugControl();
     
-	IUFillText(&SourceCodeRepositoryURLT[0], "REPOSITORY_URL", "Source Code Repository", "https://github.com/kneo/indi-bresserexos2");
+	IUFillText(&SourceCodeRepositoryURLT[0], "REPOSITORY_URL", "Code Repository", "https://github.com/kneo/indi-bresserexos2");
 	
     IUFillTextVector(&SourceCodeRepositoryURLTP, SourceCodeRepositoryURLT, 1, getDeviceName(), "REPOSITORY_URL", "Source Code", CONNECTION_TAB, IP_RO, 0, IPS_IDLE);
 	
